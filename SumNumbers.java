f77699336e821992d551b5af1fff39ee85538f2e
AnKukl <131239026+AnKukl@users.noreply.github.com>
2023-08-24 09:39:50 +0200

    HW 24.08.2023

---
+++ SumNumbers.java
@@ -0,0 +1,11 @@
+public class SumNumbers {
+    public static void main(String[] args) {
+//        Алг sumNumbers:
+//        начать
+//        ввод result = a + b + c;
+//        вывод result;
+
+        int result = 3 + 9 + 14;
+        System.out.println(result);
+    }
+}
